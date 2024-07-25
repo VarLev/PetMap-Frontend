9fefe0f7deca7661d8367d3fa1f9da5e7570c8aa
levro <levro@MSI>
2024-07-25 16:23:45 -0300

    added NativeWind

--- app/index.tsx
+++ app/index.tsx
@@ -1,28 +1,17 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
-import { useRouter } from 'expo-router';
+import { View, Text, Button } from 'react-native';
+import { useRouter } from 'expo-router';;
 
 const HomeScreen = () => {
   const router = useRouter();
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Welcome to PetMap</Text>
-      <Button title="Go to Auth" onPress={() => router.push('/auth')} />
+    <View className="flex-2 items-center justify-center bg-black">
+        <Button title="Go to Auth" onPress={() => router.push('/auth')} />
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  text: {
-    fontSize: 24,
-    marginBottom: 20
-  }
-});
+
 
 export default HomeScreen;
