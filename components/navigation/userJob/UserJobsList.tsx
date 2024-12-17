5134c95d00608fb8e7bbcff2c2ff9d8f204d9a37
Leo Feotokis <levromf@gmail.com>
2024-12-17 14:57:52 -0300

    Add job names dictionary for improved job type display in UserJobsList

--- components/navigation/userJob/UserJobsList.tsx
+++ components/navigation/userJob/UserJobsList.tsx
@@ -26,6 +26,23 @@ const UserJobsList = () => {
     }));
   };
 
+  const selectJobName = (jobType: string) => {
+    //создать словарь для типов работт и их названий и вернуть название по ключу jobType 
+    const jobNamesDictionary: { [key: string]: string } = {
+      'UserEdit': 'Заполнение профиля пользователя',
+      'FillOnboarding': 'Заполнение онбординга',
+      'PetEdit': 'Заполнение профиля питомца',
+      'AddFirstWalk': 'Добавление первой прогулки',
+      'AddMapPoint': 'Добавление публичной метки на карте',
+      'AddMapPointComment': 'Добавление комментария',
+      'GetMapBonuses': 'Собрать бонус на карте',
+      // Add more job types as needed
+    };
+
+    return jobNamesDictionary[jobType];
+
+  }
+
   const renderJobTypeCard = ({ item }: { item: JobTypeDto }) => {
     const isExpanded = expandedTypes[item.jobType] || false;
     const cardStyle = item.isCompleted ? 'bg-gray-300' : 'bg-white';
@@ -33,14 +50,14 @@ const UserJobsList = () => {
     return (
       <View className={`rounded-lg p-4 my-2 mx-4 ${cardStyle}`}>
         <TouchableOpacity onPress={() => toggleExpand(item.jobType)}>
-          <View className="flex-row items-center justify-between">
-            <View className="flex-row items-center">
+          <View className="items-start justify-start">
+            <View className="flex-row items-start">
               {isExpanded ? (
                 <IconSelectorComponent iconName="chevron-down-circle-outline" iconSet="Ionicons" />
               ) : (
                 <IconSelectorComponent iconName="chevron-up-circle-outline" iconSet="Ionicons" />
               )}
-              <Text className="text-lg font-nunitoSansBold ml-2">{item.jobTypeName}</Text>
+              <Text className="text-base font-nunitoSansBold ml-2">{selectJobName(item.jobTypeName)}</Text>
             </View>
             <Text className="text-base text-gray-600 font-nunitoSansRegular">
               {i18n.t('UserJobsList.reward')}: {item.totalBenefits}
